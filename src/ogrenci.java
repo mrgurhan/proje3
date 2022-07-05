4a2bc90ada4f22e9573c1ed9cbdd414a94ce5a44
lenovo <git config--global user.email "gurhanatas35@gmail.com>
2022-07-05 21:17:20 +0300

    test 1

--- src/ogrenci.java
+++ src/ogrenci.java
@@ -2,5 +2,6 @@ public class ogrenci {
     public static void main(String[] args) {
         System.out.println("merhaba dünya 2");
         System.out.println("merhaba dünya 3");
+        System.out.println("merhaba dünya 4");
     }
 }
